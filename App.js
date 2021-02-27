8c5b8aeaee23603db60fca0b252d33ce976aca7e
unknown <luo_anita@yahoo.com>
2021-02-27 15:06:10 -0600

    first commit

---
+++ App.js
@@ -0,0 +1,46 @@
+import { StatusBar } from 'expo-status-bar';
+import React, {PureComponent} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Entities from './src/entities'
+import { GameEngine } from 'react-native-game-engine';
+
+export default class App extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      running: true,
+    };
+    this.gameEngine = null;
+    console.disableYellowBox = true;
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <GameEngine
+          ref={ref => {
+            this.gameEngine = ref;
+          }}
+          style={styles.gameContainer}
+          entities={Entities()}
+          running={this.state.running}>
+          <StatusBar hidden={true} />
+        </GameEngine>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+  },
+  gameContainer: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+});
